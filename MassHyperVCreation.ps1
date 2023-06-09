6a30c906adda6b6f3a598b8f2ee6a6f0e00979e3
Kelley Parker <kelley.parker07@gmail.com>
2023-06-09 15:35:39 -0500

    Create MassHyperVCreation.ps1

---
+++ MassHyperVCreation.ps1
@@ -0,0 +1,45 @@
+# Create 1 VM per iso located in a specific folder.  For me, I use D:\isos.  Change $ISO path to the folder that has all your ISOs.  Change VMpath as well if you don't want to store your VMs on C:\VMs
+# Set variables
+$MemoryStartupBytes = 2GB
+$NewVHDSizeBytes = 40GB
+$ISOPath = "D:\isos"
+$VMPath = "C:\VMs"
+
+# Loop through each ISO and create a VM
+Get-ChildItem $ISOPath -Filter *.iso | ForEach-Object {
+    $iso = $_
+    # Set VM name
+    $VMName = $iso.BaseName.Replace("_VM", "")
+    # Set new VHD path
+    $NewVHDPath = Join-Path -Path $VMPath -ChildPath "$VMName.vhdx"
+
+    # Create new VM
+    New-VM -Name $VMName -MemoryStartupBytes $MemoryStartupBytes -Path $VMPath
+
+    # Create new virtual hard disk
+    New-VHD -Path $NewVHDPath -SizeBytes $NewVHDSizeBytes -Dynamic
+
+    # Attach virtual hard disk to VM
+    Add-VMHardDiskDrive -VMName $VMName -Path $NewVHDPath
+
+    # Set network adapter for VM
+    Add-VMNetworkAdapter -VMName $VMName -SwitchName "Default Switch"
+
+    # Find the appropriate ISO file based on the VM name
+    $ISOName = "$VMName.iso"
+    $ISOPathWithName = Join-Path $ISOPath $ISOName
+
+    # Set DVD drive to the appropriate ISO file
+    if(Test-Path $ISOPathWithName) {
+        Set-VMDvdDrive -VMName $VMName -Path $ISOPathWithName -Confirm:$false
+    }
+    else {
+        Write-Warning "ISO file not found: $ISOPathWithName"
+    }
+}
+
+# Start all virtual machines
+Get-VM | ForEach-Object {
+    $VM = $_
+    Start-VM -VMName $VM.Name
+}
